af70badeb6c1ea546c6f337d354af5030bc0843a
Maria Eduarda <101299266+mariadev2@users.noreply.github.com>
2023-06-01 15:16:48 -0300

    teste3

--- desafio01.py
+++ desafio01.py
@@ -1,4 +1,5 @@
 lista_deposito = []
+lista_saque = []
 saldo = 0
 deposito =0
 limite_saque_vezes_cont = 0
@@ -18,24 +19,24 @@ while True:
 
     if opcao == 1:
         saldo_anterior = deposito
-        print(f"Extrato\n Seus depositos anteriores foi R$ {lista_deposito}\nseu saldo é R$ {saldo:.2f}")
-
+        print(f"Extrato\n Seus depositos anteriores foi R$ {lista_deposito}\nSeus daques anteriores foi R$ {lista_saque}\nseu saldo é R$ {saldo:.2f}")
     elif opcao == 2:
             deposito = int(input("seu deposito:"))
             lista_deposito.append(deposito)
             saldo += deposito
             print(f"\nSeu saldo atual é {saldo}\n")
-            print(f"Extrato\n Seus depositos anteriores foi R$ {lista_deposito}\nseu saldo é R$ {saldo:.2f}")
+            
 
     elif opcao == 3:
          if limite_saque_vezes_cont <= limite_saque_vezes:
             saque = int(input("Qual valor do saque"))
-            if saque <= saldo and saque < limite_saque:
+            if saque <= saldo or saque < limite_saque:
+                lista_saque.append(saque)
                 saldo -= saque
                 limite_saque_vezes_cont += 1
                 print(f"\nSeu saldo atual é {saldo}\n")
             if saque > saldo:
-                print("negativo")
+                print("Saldo insuficiente")
          if limite_saque_vezes_cont > limite_saque_vezes:
              print("Ja ultrapassou 3 saques")
     elif opcao== 4:
