65bca65f234f925337d44bf5ec29c0ed4c563e4b
Maria Eduarda <101299266+mariadev2@users.noreply.github.com>
2023-06-05 18:52:23 -0300

    mais aula

---
+++ atividade.py
@@ -0,0 +1,36 @@
+'''C = int(input()) 
+for i in range (C): 
+    nivel_enegia = int(input())
+    if nivel_enegia <= 8000:
+        print("Inseto!")
+    else:
+        print("Mais de 8000!")a = input() 
+b = input() 
+c = input() 
+
+if a == 'vertebrado': 
+   if b == 'ave':
+      if c == 'carnivoro':
+         print("aguia")
+      elif c== 'onivoro':
+         print('pomba')
+   elif b == 'mamifero':
+      if c == 'onivoro':
+         print('homem')
+      elif c == 'herbivoro':
+         print('vaca')
+      
+elif a == 'invertebrado':
+    if b == 'inseto':
+      if c == 'hematofago':
+         print('pulga')
+      elif c == 'herbivoro':
+         print('lagarta')
+    elif b == 'analideo':
+      if c == 'hematofago':
+         print('sanguessuga')
+      elif c == 'onivoro':
+         print('minhoca')
+      '''
+
+
---
+++ lista.py
@@ -0,0 +1,10 @@
+matriz[
+    [1,2,3]
+    ["a","b","c"]
+]
+lista ["a","c","d","a","g"]
+lista.clear()
+lista.append
+
+
+
