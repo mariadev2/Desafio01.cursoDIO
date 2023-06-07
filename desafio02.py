eede7936fc53418e455d3e97cbe8117a15a757c4
Maria Eduarda <101299266+mariadev2@users.noreply.github.com>
2023-06-07 19:00:22 -0300

    https://

--- desafio02.py
+++ desafio02.py
@@ -8,12 +8,13 @@ deposito = 0
 extrato_saque = []
 extrato_deposito = []
 #funções
-def Cadastrar_usuario():
-def abrir_conta():
+
+
 def realizar_deposito (*, deposito):
     global saldo
     if deposito > 0:
         saldo += deposito
+        extrato_deposito.append(deposito)   
         return saldo
     else:
         print("insira um valor acima de 0")
@@ -29,26 +30,33 @@ def realizar_saque(*,saque):
     else:
         limite_vezes += 1
         saldo -= saque
-    return saldo
-
-def exibir_extrato(saque, deposito, extrato_saque, extrato_deposito):
-    extrato_saque = extrato_saque.append(saque)
-    extrato_deposito = extrato_deposito.append(deposito)
-    return extrato_saque, extrato_deposito
+        extrato_saque.append(saque)   
+        return saldo
 
 #funcionamento
 while True:
     print(f"Seu saldo atual é {saldo} \n")
-    opcao_selecionada = int(input("MENU\n[1]Deposito\n[2]Saque\n[3]Extrato\n"))
+    opcao_selecionada = int(input("MENU\n[1]Deposito\n[2]Saque\n[3]Extrato\n[4]Sair\n"))
+
     if opcao_selecionada == 2:
         saldo = realizar_saque(saque = int(input("Valor a ser sacado\n")))
         print(f"{limite_vezes} Saque\n")
+
     elif opcao_selecionada == 1:
         saldo = realizar_deposito(deposito= int(input("Valor a ser depositado\n")))
+
     elif opcao_selecionada == 3:
-        extrato_saque = exibir_extrato(extrato_saque,extrato_deposito)
-        extrato_deposito = exibir_extrato(extrato_deposito, extrato_saque)
-        print(f"{extrato_deposito} {extrato_saque}")
-    else:
+        print("EXTRATO\n")
+        print("Saques")
+        for indice, elemento in enumerate(extrato_saque):
+            print(f"{indice} . R$ {elemento}\n")
+        print("Depositos")
+        for indice, elemento in enumerate(extrato_deposito):
+            print(f"{indice} . R$ {elemento}\n")
+
+    elif opcao_selecionada == 4:
         break
+    else:
+        print("Opção invalida\n")
+        continue
         
