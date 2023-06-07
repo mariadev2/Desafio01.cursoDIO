32e3f6aeb6106f5340b8f7abf8cdf7de91a421ac
Maria Eduarda <101299266+mariadev2@users.noreply.github.com>
2023-06-06 21:03:26 -0300

    mais um comit

--- desafio02.py
+++ desafio02.py
@@ -3,9 +3,13 @@ limite_vezes=0
 limite_vezes_permitido =3 
 limite_valor =500
 saldo = 0
+saque = 0
+deposito = 0
 extrato_saque = []
 extrato_deposito = []
 #funções
+def Cadastrar_usuario():
+def abrir_conta():
 def realizar_deposito (*, deposito):
     global saldo
     if deposito > 0:
@@ -27,23 +31,23 @@ def realizar_saque(*,saque):
         saldo -= saque
     return saldo
 
-def exibir_extrato():
-    global extrato_saque, extrato_deposito
+def exibir_extrato(saque, deposito, extrato_saque, extrato_deposito):
     extrato_saque = extrato_saque.append(saque)
     extrato_deposito = extrato_deposito.append(deposito)
     return extrato_saque, extrato_deposito
+
 #funcionamento
 while True:
-    print(f"Seu saldo atual é {saldo}")
+    print(f"Seu saldo atual é {saldo} \n")
     opcao_selecionada = int(input("MENU\n[1]Deposito\n[2]Saque\n[3]Extrato\n"))
     if opcao_selecionada == 2:
-        saldo = realizar_saque(saque = int(input("saque")))
-        print(limite_vezes)
+        saldo = realizar_saque(saque = int(input("Valor a ser sacado\n")))
+        print(f"{limite_vezes} Saque\n")
     elif opcao_selecionada == 1:
-        saldo = realizar_deposito(deposito= int(input("Seu deposito")))
+        saldo = realizar_deposito(deposito= int(input("Valor a ser depositado\n")))
     elif opcao_selecionada == 3:
-        extrato_saque = exibir_extrato()
-        extrato_deposito = exibir_extrato()
+        extrato_saque = exibir_extrato(extrato_saque,extrato_deposito)
+        extrato_deposito = exibir_extrato(extrato_deposito, extrato_saque)
         print(f"{extrato_deposito} {extrato_saque}")
     else:
         break
